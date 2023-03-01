04199d640c6165460a415a61e86da36488cfe949
SergioWaters <mokretsovsa@gmail.com>
2023-03-01 22:10:51 +0300

    first part added

---
+++ learnjs/chapter_1/4/index.js
@@ -0,0 +1,8 @@
+'use strict'
+/**
+  * Простая страница
+  * Создайте страницу, которая спрашивает имя у пользователя и выводит его.
+*/
+
+const userName = prompt("Put your name");
+alert(userName + ' - saved as your name');
