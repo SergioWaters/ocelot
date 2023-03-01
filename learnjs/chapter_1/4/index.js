a5b2472f1b741a5a36b80a06c90e64d1058337ad
SergioWaters <mokretsovsa@gmail.com>
2023-03-01 22:18:35 +0300

    init

--- learnjs/chapter_1/4/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-/**
-  * Простая страница
-  * Создайте страницу, которая спрашивает имя у пользователя и выводит его.
-*/
-
-const userName = prompt("Put your name");
-alert(userName + ' - saved as your name');
